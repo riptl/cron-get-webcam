b31964372c3f43d50baec818692261d05456491c
terorie <terorie@alphakevin.club>
2019-02-05 00:36:48 +0100

    Initial script

---
+++ cron-get-webcam.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import calendar
+import email.utils
+import mimetypes
+import os.path
+import requests
+import shutil
+import sys
+import time
+
+mimetypes.init()
+
+if len(sys.argv) != 3:
+	sys.exit("Usage ./cron-webcam-pic.py <dir> <url>")
+
+prefix = sys.argv[1]
+url    = sys.argv[2]
+
+# Get metadata through HEAD request
+head_r = requests.head(url)
+last_modified = head_r.headers['last-modified']
+content_type  = head_r.headers['content-type']
+timestamp = email.utils.parsedate(last_modified)
+extension = mimetypes.guess_extension(content_type)
+
+# Generate file path
+t = timestamp
+day = '%d-%02d-%02d' % (t[0], t[1], t[2])
+clock = '%02d%02d%02d' % (t[3], t[4], t[5]) + extension
+dirs = os.path.join(prefix, day)
+fullpath = os.path.join(dirs, clock)
+
+# Check if file exists
+if os.path.exists(fullpath):
+	sys.exit(0)
+
+# Download new file
+os.makedirs(dirs, exist_ok=True)
+r = requests.get(url, stream=True)
+with open(fullpath, 'wb') as f:
+	shutil.copyfileobj(r.raw, f)
+
+# Set modification time
+unix = calendar.timegm(timestamp)
+os.utime(fullpath, (unix, unix))
